f6015bd7caa40f0cac1ce4d99cccea4db99d993b
carlodev <carlo.toribio.dev@gmail.com>
2025-02-11 19:35:18 -0800

    Initial Commit.

---
+++ CSharpLearningProject/Program.cs
@@ -0,0 +1,80 @@
+﻿//variables
+
+// char - 'a'
+// string - "Hello, Kevin!"
+// int - 0
+// float - 0.1f
+// double, decimal - 0.1
+// bool - true, false
+
+//
+// Lesson #1: Print "Hello, World!"
+//
+
+string myName = "Kevin Mulby ";
+string carlo = "Carlo";
+
+// calling the function
+SampleFunction(carlo);
+
+// definition
+void SampleFunction(string name)
+{
+    Console.WriteLine("Hello from function " + name);
+}
+
+//
+// Lesson #2: Functions to add, subtract, multiply and divide Integers (int)
+//
+
+// Function examples
+int userNumber1 = 10;
+int userNumber2 = 20;
+
+int addition = AddTwoNumbers(userNumber1, userNumber2);
+Console.WriteLine("The addition is " + addition);
+
+int multiplication = MultiplyTwoNumbers(userNumber1, userNumber2);
+Console.WriteLine("The multiplication is " + multiplication);
+
+int division = DivideTwoNumbers(userNumber1, userNumber2);
+Console.WriteLine("The division is " + division);
+
+int division2 = DivideTwoNumbers(userNumber2, userNumber1);
+Console.WriteLine("The 2nd division is " + division2);
+
+for (int i = 0; i < 25; i++)
+{
+    AddTwoNumbers(10, i);
+}
+
+int AddTwoNumbers(int n1, int n2)
+{
+    Console.WriteLine("Adding " + n1 + " + " + n2);
+    int total = n1 + n2;
+    return total;
+}
+
+int MultiplyTwoNumbers(int n1, int n2)
+{
+    Console.WriteLine("Multiplying " + n1 + " * " + n2);
+    int total = n1 * n2;
+    return total;
+}
+
+int DivideTwoNumbers(int n1, int n2, int multiplier = 3)
+{
+    Console.WriteLine("Dividing " + n1 + " / " + n2);
+    int total = n1 / n2;
+    
+    return total;
+}
+
+//
+// Lesson #3: Functions that take other parameter types
+//
+
+// int SayHiOrSayBye(bool sayHi, bool sayBye, string name, int byeCount)
+// {
+
+// }  
